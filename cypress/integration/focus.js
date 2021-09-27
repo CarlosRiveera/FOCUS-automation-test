2e17bf199ce340564a1f6f4a04c65305b6d7d283
carlosriveera <carlosriveera97@gmail.com>
2021-09-26 22:48:57 -0600

    Locate North America Link step

--- cypress/integration/focus.js
+++ cypress/integration/focus.js
@@ -1,18 +1,34 @@
 describe('Google Search', () => {
-    it('loads search page', () => {
-      cy.visit('https://www.google.com');
-    });
-  
-    it('searc `Focus Services`', () => {
-      cy.get('input[name="q"]').type('Focus Services{enter}');
-    });
-  
-    it('gets FOCUS url site', () => {
-      
-      if(cy.get('[href="https://www.focusservices.com/"] > .TbwUpd > .iUh30')){
-            cy.log('found it')
-        } else {
-            cy.log('not found')
-        }
-    });
+
+  it('go to google', () => {
+    cy.visit('https://www.google.com');
   });
+
+  it('Type on google search `Focus Services`', () => {
+    cy.get('input[name="q"]').type('Focus Services{enter}');
+  });
+
+  it('verify URL of focus exist', () => {
+    if(cy.get('[href="https://www.focusservices.com/"] > .TbwUpd > .iUh30')){
+      cy.log('it does exist')
+    } else {
+      cy.log('doesnt exist')
+    }
+  });
+
+  it('Scroll down to the bottom of the page and verify Now Hiring', () => {
+    cy.get('[href="https://www.focusservices.com/"] > .LC20lb').first().click()
+    cy.scrollTo('bottom')
+    if(cy.get('span').contains('Now Hiring!')){
+      cy.log('it does exist')
+      
+    } else {
+      cy.log('doesnt exist')
+    }
+  });
+
+  it('Locate North America Link', () => {
+    cy.get('span').contains('Locations').first().click()
+  });
+
+});
