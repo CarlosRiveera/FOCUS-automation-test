b9fb76bfa44850aad568d964ce1872fe380c1aa6
carlosriveera <carlosriveera97@gmail.com>
2021-09-27 00:23:56 -0600

    Adding last test validation

--- cypress/integration/focus.js
+++ cypress/integration/focus.js
@@ -1,6 +1,6 @@
 describe('Google Search', () => {
 
-  it('go to google', () => {
+  it('Go to google', () => {
     cy.visit('https://www.google.com');
   });
 
@@ -8,9 +8,9 @@ describe('Google Search', () => {
     cy.get('input[name="q"]').type('Focus Services{enter}');
   });
 
-  it('verify URL of focus exist', () => {
+  it('Verify URL of focus exist', () => {
     if(cy.get('[href="https://www.focusservices.com/"] > .TbwUpd > .iUh30')){
-      cy.log('it does exist')
+      cy.log('Link does exist')
     } else {
       cy.log('doesnt exist')
     }
@@ -20,7 +20,8 @@ describe('Google Search', () => {
     cy.get('[href="https://www.focusservices.com/"] > .LC20lb').first().click()
     cy.scrollTo('bottom')
     if(cy.get('span').contains('Now Hiring!')){
-      cy.log('it does exist')
+      cy.log('Now Hiring does exist')
+      cy.get('span').contains('Locations').first().click()
       
     } else {
       cy.log('doesnt exist')
@@ -28,7 +29,18 @@ describe('Google Search', () => {
   });
 
   it('Locate North America Link', () => {
-    cy.get('span').contains('Locations').first().click()
+    cy.get('a[href="#north-america"]').should('exist')
+    cy.get('a[href="#central-america"]').click()
   });
+  
+  it('Validate “El Salvador & Nicaragua” title ', () => {
+    cy.get('h2').contains('El Salvador & Nicaragua').should('exist')
+    cy.get('a[href="#asia"]').click()
+  });
+
+  it('Verify if exist the “Bacolod City" ', () => {
+    cy.get('body').contains('Bacolod City, Philippines').should('exist')
+  });
+
 
 });
